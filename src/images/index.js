6152cf9a33985efbc58bde51df78a9785038f431
Kashish8888 <kashish@unthinkable.co>
2023-11-23 10:27:20 +0530

    added icons

--- src/images/index.js
+++ src/images/index.js
@@ -1,17 +1,37 @@
-const avatar = '/images/Avatar.svg';
-const logo = '/images/logo.png';
-const sun = '/images/sun.png';
-const moon = '/images/moon.png';
-const flagFr = '/images/flag-french.png';
-const flagUk = '/images/flag-uk.png';
-const homeSolid = '/images/home-solid.png';
-const homeOutline = '/images/home-outline.png';
-const profileSolid = '/images/profile-solid.png';
-const profileOutline = '/images/profile-outline.png';
-const cmibIcon ='/images/svg/cmibIcon.svg';
-const azadiMohatsav ='/images/svg/azadiMohatsav.svg';
-const g20Icon ='/images/svg/g20Icon.svg';
-const gloPac ='/images/svg/gloPac.svg';
+const avatar = "/images/Avatar.svg";
+const logo = "/images/logo.png";
+const sun = "/images/sun.png";
+const moon = "/images/moon.png";
+const flagFr = "/images/flag-french.png";
+const flagUk = "/images/flag-uk.png";
+const homeSolid = "/images/home-solid.png";
+const homeOutline = "/images/home-outline.png";
+const profileSolid = "/images/profile-solid.png";
+const profileOutline = "/images/profile-outline.png";
+const cmibIcon = "/images/svg/cmibIcon.svg";
+const azadiMohatsav = "/images/svg/azadiMohatsav.svg";
+const g20Icon = "/images/svg/g20Icon.svg";
+const gloPac = "/images/svg/gloPac.svg";
+const iconWalkthroughSignUpOne =
+  "/images/iconWalkthroughSignUpOne/iconWalkthroughSignUpOne.png";
+const iconCheckedCheckbox =
+  "/images/iconCheckedCheckbox/iconCheckedCheckbox.png";
+const iconUncheckedCheckbox =
+  "images/iconUncheckedCheckbox/iconUncheckedCheckbox.png";
+const iconArrowRightWhite =
+  "images/iconArrowRightWhite/iconArrowRightWhite.png";
+const iconWalkthroughSignUpTwo =
+  "/images/iconWalkthroughSignUpTwo/iconWalkthroughSignUpTwo.png";
+const iconDownArrow = "/images/iconDownArrow/iconDownArrow.png";
+const iconArrowLeft = "/images/iconArrowLeft/iconArrowLeft.png";
+const iconWalkthroughSignUpThree =
+  "/images/iconWalkthroughSignUpThree/iconWalkthroughSignUpThree.png";
+const iconWalkthroughSignUpLast =
+  "/images/iconWalkthroughSignUpLast/iconWalkthroughSignUpLast.png";
+const iconDivider = "/images/iconDivider/iconDivider.png";
+const iconUpload = "/images/iconUpload/iconUpload.png";
+const iconTrash = "/images/iconTrash/iconTrash.png";
+const iconSuccess = "/images/iconSuccess/iconSuccess.png";
 
 export default {
   avatar,
@@ -27,5 +47,18 @@ export default {
   cmibIcon,
   azadiMohatsav,
   g20Icon,
-  gloPac
+  gloPac,
+  iconWalkthroughSignUpOne,
+  iconCheckedCheckbox,
+  iconUncheckedCheckbox,
+  iconArrowRightWhite,
+  iconWalkthroughSignUpTwo,
+  iconDownArrow,
+  iconArrowLeft,
+  iconWalkthroughSignUpThree,
+  iconWalkthroughSignUpLast,
+  iconDivider,
+  iconUpload,
+  iconTrash,
+  iconSuccess,
 };
--- src/images/index.native.js
+++ src/images/index.native.js
@@ -1,13 +1,26 @@
-import avatar from '../../public/images/Avatar.svg';
-import logo from '../../public/images/logo.png';
-import sun from '../../public/images/sun.png';
-import moon from '../../public/images/moon.png';
-import flagFr from '../../public/images/flag-french.png';
-import flagUk from '../../public/images/flag-uk.png';
-import homeSolid from '../../public/images/home-solid.png';
-import homeOutline from '../../public/images/home-outline.png';
-import profileSolid from '../../public/images/profile-solid.png';
-import profileOutline from '../../public/images/profile-outline.png';
+import avatar from "../../public/images/Avatar.svg";
+import logo from "../../public/images/logo.png";
+import sun from "../../public/images/sun.png";
+import moon from "../../public/images/moon.png";
+import flagFr from "../../public/images/flag-french.png";
+import flagUk from "../../public/images/flag-uk.png";
+import homeSolid from "../../public/images/home-solid.png";
+import homeOutline from "../../public/images/home-outline.png";
+import profileSolid from "../../public/images/profile-solid.png";
+import profileOutline from "../../public/images/profile-outline.png";
+import iconWalkthroughSignUpOne from "../../public/images/iconWalkthroughSignUpOne/iconWalkthroughSignUpOne.png";
+import iconCheckedCheckbox from "../../public/images/iconCheckedCheckbox/iconCheckedCheckbox.png";
+import iconUncheckedCheckbox from "../../public/images/iconUncheckedCheckbox/iconUncheckedCheckbox.png";
+import iconArrowRightWhite from "../../public/images/iconArrowRightWhite/iconArrowRightWhite.png";
+import iconWalkthroughSignUpTwo from "../../public/images/iconWalkthroughSignUpTwo/iconWalkthroughSignUpTwo.png";
+import iconDownArrow from "../../public/images/iconDownArrow/iconDownArrow.png";
+import iconArrowLeft from "../../public/images/iconArrowLeft/iconArrowLeft.png";
+import iconWalkthroughSignUpThree from "../../public/images/iconWalkthroughSignUpThree/iconWalkthroughSignUpThree.png";
+import iconWalkthroughSignUpLast from "../../public/images/iconWalkthroughSignUpLast/iconWalkthroughSignUpLast.png";
+import iconDivider from "../../public/images/iconDivider/iconDivider.png";
+import iconUpload from "../../public/images/iconUpload/iconUpload.png";
+import iconTrash from "../../public/images/iconTrash/iconTrash.png";
+import iconSuccess from "../../public/images/iconSuccess/iconSuccess.png";
 
 export default {
   avatar,
@@ -20,4 +33,17 @@ export default {
   homeOutline,
   profileSolid,
   profileOutline,
+  iconWalkthroughSignUpOne,
+  iconCheckedCheckbox,
+  iconUncheckedCheckbox,
+  iconArrowRightWhite,
+  iconWalkthroughSignUpTwo,
+  iconDownArrow,
+  iconArrowLeft,
+  iconWalkthroughSignUpThree,
+  iconWalkthroughSignUpLast,
+  iconDivider,
+  iconUpload,
+  iconTrash,
+  iconSuccess,
 };
