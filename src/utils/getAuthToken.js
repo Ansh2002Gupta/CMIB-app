051181a51a9a27acd09bd256b559dfbbc034c5da
Neha Mondal <neha.mondal@unthinkable.co>
2023-12-06 16:32:03 +0530

    removed unrequired checks on return statement

--- src/utils/getAuthToken.js
+++ src/utils/getAuthToken.js
@@ -3,11 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const getAuthToken = async () => {
   try {
     const authToken = await AsyncStorage.getItem('authToken');
-    if (authToken !== null) {
-      return authToken;
-    } else {
-      return null;
-    }
+    return authToken;
   } catch (error) {
     throw error;
   }
