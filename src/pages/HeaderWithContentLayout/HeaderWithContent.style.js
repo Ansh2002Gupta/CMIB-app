0d9430333d13073e2c7593a022915b0a04973fc4
Neha Mondal <neha.mondal@unthinkable.co>
2023-12-06 16:30:35 +0530

    made a separate style file for header

---
+++ src/pages/HeaderWithContentLayout/HeaderWithContent.style.js
@@ -0,0 +1,12 @@
+import colors from "../../assets/colors";
+
+const style = {
+  headerContainer: {
+    position: "sticky",
+    top: 0,
+    zIndex: 1,
+    backgroundColor: colors.white,
+  },
+};
+
+export default style;
--- src/pages/HeaderWithContentLayout/HeaderWithContentLayout.js
+++ src/pages/HeaderWithContentLayout/HeaderWithContentLayout.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
-import { Outlet } from "../routes";
-import MainLayout from "../layouts/MainLayout";
-import LoginHeaderContainer from "../containers/LoginHeader";
+import { Outlet } from "../../routes";
+import MainLayout from "../../layouts/MainLayout";
+import LoginHeaderContainer from "../../containers/LoginHeader";
 import { MediaQueryContext } from "@unthinkable/react-theme";
+
+import style from "./HeaderWithContent.style"
 
 function HeaderWithContentLayout() {
   const { current: currentBreakpoint } = useContext(MediaQueryContext);
@@ -12,7 +14,7 @@ function HeaderWithContentLayout() {
     <MainLayout
       header={<LoginHeaderContainer />}
       content={<Outlet />}
-      topSectionStyle={{position: 'sticky', top: 0, zIndex: 1, backgroundColor: "white"}}
+      topSectionStyle={style.headerContainer}
     />
   );
 }
