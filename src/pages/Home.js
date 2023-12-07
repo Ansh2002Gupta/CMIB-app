4229e47a5b3d06df3f3f3a9e2b0829dca333eb5a
AkshatJain <akshat@unthinkable.co>
2023-12-07 12:38:46 +0530

    implement check for web

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Outlet } from "../routes";
+import { MediaQueryContext } from "@unthinkable/react-theme";
 
 import MainLayout from "../layouts/MainLayout";
 import HeaderContainer from "../containers/Header";
 import BottomBar from "../containers/BottomBar";
 
 function Home() {
+  const { current: currentBreakpoint } = useContext(MediaQueryContext);
+  const isWebView = currentBreakpoint !== "xs";
+
   return (
     <MainLayout
       header={<HeaderContainer />}
-      menu={<BottomBar />}
+      menu={!isWebView ? <BottomBar /> : null}
       content={<Outlet />}
     />
   );
