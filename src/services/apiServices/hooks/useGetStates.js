6d4d5462345fbc787fd48c7e3e0a806f39971dfc
Kashish8888 <kashish@skizaa.com>
2023-12-12 21:38:45 +0530

    naming fix

--- src/services/apiServices/hooks/useGetStates.js
+++ src/services/apiServices/hooks/useGetStates.js
@@ -5,23 +5,25 @@ import { API_STATUS, STATUS_CODES } from "../../../constants/constants";
 import { GENERIC_GET_API_FAILED_ERROR_MESSAGE } from "../../../constants/errorMessages";
 
 const useGetStates = () => {
-  const [getStatus, setGetStatus] = useState(API_STATUS.IDLE);
+  const [gettingStatesStatus, setGettingStatesStatus] = useState(
+    API_STATUS.IDLE
+  );
   const [stateResult, setStateResult] = useState(null);
   const [error, setError] = useState("");
 
   const getStates = async () => {
     try {
-      setGetStatus(API_STATUS.LOADING);
+      setGettingStatesStatus(API_STATUS.LOADING);
       setError("");
       const res = await Http.get(`core/all-states`);
       if (res.code === STATUS_CODES.SUCCESS_STATUS) {
-        setGetStatus(API_STATUS.SUCCESS);
+        setGettingStatesStatus(API_STATUS.SUCCESS);
         setStateResult(res.data);
         return;
       }
-      setGetStatus(API_STATUS.ERROR);
+      setGettingStatesStatus(API_STATUS.ERROR);
     } catch (err) {
-      setGetStatus(API_STATUS.ERROR);
+      setGettingStatesStatus(API_STATUS.ERROR);
       if (err.response?.data?.message) {
         setError(err.response?.data?.message);
         return;
@@ -30,9 +32,9 @@ const useGetStates = () => {
     }
   };
 
-  const isLoading = getStatus === API_STATUS.LOADING;
-  const isSuccess = getStatus === API_STATUS.SUCCESS;
-  const isError = getStatus === API_STATUS.ERROR;
+  const isLoading = gettingStatesStatus === API_STATUS.LOADING;
+  const isSuccess = gettingStatesStatus === API_STATUS.SUCCESS;
+  const isError = gettingStatesStatus === API_STATUS.ERROR;
 
   return {
     error,
@@ -41,7 +43,7 @@ const useGetStates = () => {
     isError,
     isLoading,
     isSuccess,
-    getStatus,
+    gettingStatesStatus,
   };
 };
 
