a1b9de9592d87a30c32ca72b015f276f675f0177
Kashish8888 <kashish@skizaa.com>
2023-12-19 17:27:03 +0530

    updated style format

--- src/components/BadgeLabel/BadgeLabel.style.js
+++ src/components/BadgeLabel/BadgeLabel.style.js
@@ -5,14 +5,18 @@ const style = {
     flexDirection: "row",
     flexWrap: "wrap",
   },
-  containerStyle: { paddingBottom: 16 },
+  containerStyle: {
+    paddingBottom: 16,
+  },
   innerContainerStyle: {
     backgroundColor: colors.secondaryGrey,
     justifyContent: "center",
     borderRadius: 16,
     alignSelf: "flex-start",
   },
-  webInnerContainer: { marginRight: 8 },
+  webInnerContainer: {
+    marginRight: 8,
+  },
   badgeStyle: {
     fontSize: 14,
     paddingRight: 16,
--- src/views/CompanyProfile/CompanyProfile.style.js
+++ src/views/CompanyProfile/CompanyProfile.style.js
@@ -5,8 +5,13 @@ const style = {
     flex: 1,
     backgroundColor: colors.backgroundColor,
   },
-  innerContainerStyle: { marginBottom: 16 },
-  textContainer: { flexDirection: "row", paddingBottom: 16 },
+  innerContainerStyle: {
+    marginBottom: 16,
+  },
+  textContainer: {
+    flexDirection: "row",
+    paddingBottom: 16,
+  },
   headingText: {
     fontSize: 16,
     color: colors.darkGrey,
