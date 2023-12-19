b5f6b930e7b4a5c3f1bcec5880c75529fcf48e56
Joginder Singh <joginder.singh@daffodilsw.com>
2023-12-19 20:19:26 +0530

    remove comment line

--- src/components/TwoRowButton/twoRowButton.style.js
+++ src/components/TwoRowButton/twoRowButton.style.js
@@ -2,11 +2,9 @@ import colors from "../../assets/colors";
 
 const style = {
   containerStyle: {
-    // Add styles for the container here
     marginTop: 24,
   },
   disableButtonStyle: {
-    // Add styles for the first button here
     height: 56,
     backgroundColor: colors.white,
     justifyContent: "center",
@@ -18,13 +16,11 @@ const style = {
     flex: 1,
   },
   disableTextStyle: {
-    // Add text styles for the first button here
     fontSize: 16,
     color: colors.black,
     margin: 8,
   },
   buttonStyle: {
-    // Add styles for the second button here
     height: 56,
     backgroundColor: colors.green,
     justifyContent: "center",
@@ -34,12 +30,10 @@ const style = {
     flex: 1,
   },
   secondButtonStyle: {
-    // Add additional styles for the second button here
     marginLeft: 8,
     backgroundColor: colors.yellow,
   },
   titleStyle: {
-    // Add text styles for the second button here
     fontSize: 14,
     color: colors.black,
   },
