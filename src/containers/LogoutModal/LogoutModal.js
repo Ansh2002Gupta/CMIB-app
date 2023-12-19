2cea3fa15882a428c1b234660248c27b29739e2a
Joginder Singh <joginder.singh@daffodilsw.com>
2023-12-20 05:15:16 +0530

    pr comment resolved

--- src/containers/LogoutModal/LogoutModal.js
+++ src/containers/LogoutModal/LogoutModal.js
@@ -3,21 +3,21 @@ import { useIntl } from "react-intl";
 import PropTypes from "prop-types";
 
 import images from "../../images";
-import CommonText from "../CommonText";
+import CommonText from "../../components/CommonText";
 import CustomModal from "../../components/CustomModal/CustomModal";
 import MultiRow from "../../core/layouts/MultiRow";
-import TwoRowButton from "../TwoRowButton/TwoRowButton";
+import TwoRowButton from "../../components/TwoRowButton/TwoRowButton";
 import  useLogoutAPI from "../../services/apiServices/hooks/useLogoutAPI"
 import styles from "./logoutModal.style";
 
 const LogoutModal = ({ onCancel ,onSave}) => {
   const intl = useIntl();
   const { handleUserLogout } = useLogoutAPI();
-  const Warning = images.Warning;
+  const IconWarning = images.iconWarning;
 
   const rowConfigs = [
     {
-      content: <Warning style={styles.logo}/>,
+      content: <IconWarning style={styles.logo}/>,
     },
     {
       content:  <CommonText customTextStyle={styles.headerText} title={intl.formatMessage({ id: "label.logout" })} />,
