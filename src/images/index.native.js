2116494a91bc5e85da6d0eb23cf9c202b37a06a6
unthinkableakshat <100261062+unthinkableakshat@users.noreply.github.com>
2023-12-20 16:32:39 +0530

    resolved

--- src/images/index.native.js
+++ src/images/index.native.js
@@ -57,11 +57,8 @@ import iconAddApplicationForm from "../../public/images/iconAddApplicationForm/i
 import iconHiringProcess from "../../public/images/iconHiringProcess/iconHiringProcess.png";
 import iconDownloadDetails from "../../public/images/iconDownloadDetails/iconDownloadDetails.png";
 import iconDrawer from "../../public/images/iconDrawer/iconDrawer.png";
-<<<<<<< HEAD
 import iconTick from "../../public/images/iconTick/iconTick.png";
-=======
 import iconWarning from "../../public/images/svg/iconWarning.svg"
->>>>>>> development
 import iconSquareEdit from "../../public/images/iconSquareEdit/iconSquareEdit.png";
 
 const images = {
