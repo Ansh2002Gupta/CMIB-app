d3370088a9c184bb8860f38c7a4d62ead9375e0e
Lakshay Gupta <lakshay.gupta@unthinkable.co>
2023-12-20 12:44:29 +0000

    Revert "modal For logout,popover,edit and upload profile (pull request #28)"

--- src/core/layouts/index.js
+++ src/core/layouts/index.js
@@ -1,8 +1,7 @@
-import Base from './Base';
-import TwoColumn from './TwoColumn';
-import TwoRow from './TwoRow';
-import ThreeRow from './ThreeRow';
-import FourColumn from './FourColumn';
-import FourRow from './FourRow';
+import Base from "./Base";
+import TwoColumn from "./TwoColumn";
+import TwoRow from "./TwoRow";
+import ThreeRow from "./ThreeRow";
+import FourColumn from "./FourColumn";
 
-export { Base, TwoColumn, TwoRow, ThreeRow, FourColumn, FourRow };
+export { Base, TwoColumn, TwoRow, ThreeRow, FourColumn };
