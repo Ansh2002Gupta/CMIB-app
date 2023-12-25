e9e48774fecea6a5a038d35476ff5192dd34e034
Joginder Singh <joginder.singh@daffodilsw.com>
2023-12-25 19:54:21 +0530

    add multirow

--- src/core/layouts/MultiRow/MultiRow.js
+++ src/core/layouts/MultiRow/MultiRow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 import BaseLayout from '../Base';
 
-import layoutStyle from './MultiRow.style';
+import layoutStyle from './MultiRows.style';
 
 function MultiRow({
   style,
