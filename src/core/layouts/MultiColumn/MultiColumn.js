6d3851a0d4ae0b9ebfffaad075403ed7959df8a5
Joginder Singh <joginder.singh@daffodilsw.com>
2023-12-25 20:01:08 +0530

    add file mcolumn

--- src/core/layouts/MultiColumn/MultiColumn.js
+++ src/core/layouts/MultiColumn/MultiColumn.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import BaseLayout from "../Base";
 
-import layoutStyle from "./MultiColumn.style";
+import layoutStyle from "./MColumn.style";
 
 function MultiColumn({ style, columns }) {
   return (
