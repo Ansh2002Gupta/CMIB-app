2955db63c1045a36bc570151509ba4a2f6e1d0c2
Joginder Singh <joginder.singh@daffodilsw.com>
2023-12-25 20:03:41 +0530

    add table

--- src/core/layouts/TableLayout/TableView.js
+++ src/core/layouts/TableLayout/TableView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 import { View } from '@unthinkable/react-core-components'; 
 
-import layoutStyle from './TableView.style';
+import layoutStyle from './TablView.style';
 function TableView({ gridData, cellStyle, rowStyle, tableStyle }) {
   return (
     <View style={tableStyle}>
