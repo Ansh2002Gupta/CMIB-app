1e121bb20f4c87a6544edf2e8e23c7d773d9402a
unthinkableakshat <100261062+unthinkableakshat@users.noreply.github.com>
2023-12-26 12:50:20 +0530

    PR changes

--- src/components/OtpInput/OtpInput.js
+++ src/components/OtpInput/OtpInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import { Platform, TextInput, View } from "@unthinkable/react-core-components";
 
