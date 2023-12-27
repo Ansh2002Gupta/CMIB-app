c29e17643c6746df3b11e22cf3be5af0f2548e04
unthinkableakshat <100261062+unthinkableakshat@users.noreply.github.com>
2023-12-27 17:11:30 +0530

    changes

--- src/pages/HeaderWithContentLayout.js
+++ src/pages/HeaderWithContentLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "../routes";
 
-import Footer from "../containers/Footer/Footer";
+import Footer from "../containers/Footer";
 import Header from "../containers/Header";
 import MainLayout from "../layouts/MainLayout";
 import useIsWebView from "../hooks/useIsWebView";
