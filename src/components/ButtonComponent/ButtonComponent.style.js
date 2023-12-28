a3516ff9fbfeb2ee82dec197ddea539965e89cb2
Kashish8888 <kashish@skizaa.com>
2023-12-28 14:23:58 +0530

    seq fix

--- src/components/ButtonComponent/ButtonComponent.style.js
+++ src/components/ButtonComponent/ButtonComponent.style.js
@@ -1,5 +1,5 @@
-import colors from "../../assets/colors";
 import { StyleSheet } from "@unthinkable/react-core-components";
+import colors from "../../assets/colors";
 
 const styles = StyleSheet.create({
   buttonStyle: {
