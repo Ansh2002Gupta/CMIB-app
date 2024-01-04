13f7b057ef052efbcd12b30370881481898f9e0d
Kashish8888 <kashish@skizaa.com>
2024-01-04 20:58:45 +0530

    update console warning

--- src/containers/SignupScreens/SignUpWelcomeScreen/SignUpWelcomeScreenUI.js
+++ src/containers/SignupScreens/SignUpWelcomeScreen/SignUpWelcomeScreenUI.js
@@ -96,7 +96,7 @@ const SignUpWelcomeScreenUI = ({
           isLoading={isLoading}
           disabled={contactDetails.length <= 0}
           withGreenBackground
-          style={!isWebView && style.customButtonContainer}
+          style={!isWebView ? style.customButtonContainer : {}}
           iconRight={{
             rightIconAlt: "right-arrow",
             rightIconSource: images.iconArrowRightWhite,
