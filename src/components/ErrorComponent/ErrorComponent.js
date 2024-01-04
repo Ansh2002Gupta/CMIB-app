701cd115f5efd6070c1d5e5f475858ef6c50a7fc
Kashish8888 <kashish@skizaa.com>
2024-01-04 18:21:36 +0530

    UI fix

--- src/components/ErrorComponent/ErrorComponent.js
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -2,9 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Text, View } from "@unthinkable/react-core-components";
 
+import styles from "./ErrorComponent.style";
+
 const ErrorComponent = ({ errorMsg }) => {
   return (
-    <View>
+    <View style={styles.containerStyle}>
       <Text>{errorMsg}</Text>
     </View>
   );
--- src/components/ErrorComponent/ErrorComponent.style.js
+++ src/components/ErrorComponent/ErrorComponent.style.js
@@ -1,5 +1,11 @@
 import { StyleSheet } from "@unthinkable/react-core-components";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default styles;
