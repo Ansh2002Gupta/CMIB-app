252de26e0fa662bc8ad229bd610ce07726d44664
Joginder Singh <joginder.singh@daffodilsw.com>
2024-01-04 14:14:27 +0530

    code push

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Platform, Text, View } from "@unthinkable/react-core-components";
 
-import Button from "../Button/Button";
+import Button from "../Button";
 import CustomImage from "../CustomImage";
 import Spinner from "../Spinner";
 import colors from "../../assets/colors";
--- src/containers/LogoutModal/LogoutModal.js
+++ src/containers/LogoutModal/LogoutModal.js
@@ -1,21 +1,56 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useIntl } from "react-intl";
-import { Platform } from "@unthinkable/react-core-components";
+import { Platform, View } from "@unthinkable/react-core-components";
 
 import CommonText from "../../components/CommonText";
+import CustomButton from "../../components/CustomButton";
 import CustomImage from "../../components/CustomImage";
 import Modal from "../../components/Modal";
+import MultiColumn from "../../core/layouts/MultiColumn"
 import MultiRow from "../../core/layouts/MultiRow";
-import TwoRowButton from "../../components/TwoRowButton";
 import useLogoutAPI from "../../services/apiServices/hooks/useLogoutAPI";
 import images from "../../images";
 import styles from "./logoutModal.style";
 
 const LogoutModal = ({ onCancel, onSave }) => {
   const intl = useIntl();
-  const { handleUserLogout } = useLogoutAPI();
+  const {isLoading , handleUserLogout } = useLogoutAPI();
   const WarningIcon = images.iconWarning;
+
+  const cancelHandler = () => {
+    onCancel(false);
+  };
+
+  const saveHandler = () => {
+    handleUserLogout(() => {
+      onCancel(false);
+      onSave();
+    });
+  };
+
+  const saveCancelButton = [
+    {
+      content: (
+        <CustomButton onPress={cancelHandler}>
+          {intl.formatMessage({ id: "label.cancel" })}
+        </CustomButton>
+      ),
+      isFillSpace: true,
+    },
+    {
+      content: (
+        <CustomButton
+          onPress={saveHandler}
+          style={{ backgroundColor: "#FABB00", marginLeft: 16 }}
+          {...{isLoading}} 
+        >
+          {intl.formatMessage({ id: "label.logout" })}
+        </CustomButton>
+      ),
+      isFillSpace: true,
+    },
+  ];
 
   const logoutConfig = [
     {
@@ -46,23 +81,7 @@ const LogoutModal = ({ onCancel, onSave }) => {
     },
     {
       content: (
-        <TwoRowButton
-          leftButtonText={intl.formatMessage({ id: "label.cancel" })}
-          onLeftButtonClick={() => {
-            onCancel(false);
-          }}
-          leftTextStyle={styles.leftTextStyle}
-          leftButtonStyle={styles.cancelStyle}
-          rightButtonText={intl.formatMessage({ id: "label.logout" })}
-          rightButtonStyle={styles.saveStyle}
-          rightTextStyle={styles.rightTextStyle}
-          onRightButtonClick={() => {
-            handleUserLogout(() => {
-              onCancel(false);
-              onSave();
-            });
-          }}
-        />
+       <MultiColumn columns={saveCancelButton} />
       ),
       style: styles.gapStyle,
     },
