d8b3c883f35c1a3ab6bd951e286b96610f2b934f
Kashish8888 <kashish@skizaa.com>
2024-01-05 18:33:39 +0530

    import fix

--- src/containers/PrivateHeader/PrivateHeader.js
+++ src/containers/PrivateHeader/PrivateHeader.js
@@ -6,11 +6,11 @@ import {
   View,
 } from "@unthinkable/react-core-components";
 import { useWindowDimensions } from "@unthinkable/react-theme/src/useWindowDimensions";
-import { navigations } from "../../constants/routeNames";
 
 import CommonText from "../../components/CommonText";
 import CustomAvatar from "../../components/CustomAvatar";
 import useIsWebView from "../../hooks/useIsWebView";
+import { navigations } from "../../constants/routeNames";
 import images from "../../images";
 import styles from "./PrivateHeader.style";
 
