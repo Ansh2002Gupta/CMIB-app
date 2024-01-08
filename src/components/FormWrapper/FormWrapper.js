d6b5891c5d34f6225d0bb7ce8e767864d62731d5
NehaMondal4 <114975555+NehaMondal4@users.noreply.github.com>
2024-01-08 19:45:19 +0530

    added proptypes

--- src/components/FormWrapper/FormWrapper.js
+++ src/components/FormWrapper/FormWrapper.js
@@ -1,11 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const FormWrapper = ({ children, onSubmit, customFormStyle }) => {
+const FormWrapper = ({ children, customFormStyle, onSubmit }) => {
   return (
     <form style={customFormStyle} id="form" onSubmit={onSubmit}>
       {children}
     </form>
   );
 };
+
+FormWrapper.defaultProps = {
+  customFormStyle: {},
+}
+
+FormWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+  customFormStyle: PropTypes.object,
+  onSubmit: PropTypes.func.isRequired,
+}
 
 export default FormWrapper;
