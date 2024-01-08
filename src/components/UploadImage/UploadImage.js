99ebc52968410d7ab1b190fcaefeb16273d2fd30
Lakshay <lakshay.gupta@unthinkable.co>
2024-01-08 11:27:55 +0530

    fix- Prop type validation for errorWhileUpload in the UploadImage component

--- src/components/UploadImage/UploadImage.js
+++ src/components/UploadImage/UploadImage.js
@@ -131,7 +131,7 @@ UploadImage.defaultProps = {
 };
 
 UploadImage.propTypes = {
-  errorWhileUpload: "",
+  errorWhileUpload: PropTypes.string,
   fileUploadResult: PropTypes.object,
   handleFileUpload: PropTypes.func,
   isUploadingImageToServer: PropTypes.bool,
