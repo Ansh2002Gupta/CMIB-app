397aae21369c6b412185a2a8c042782e26a09e90
unthinkableakshat <100261062+unthinkableakshat@users.noreply.github.com>
2024-01-10 00:18:24 +0530

    working

--- src/containers/RoundOne/ApplicationFormContainer/JobDetails/controllers/useJobDetailForm.js
+++ src/containers/RoundOne/ApplicationFormContainer/JobDetails/controllers/useJobDetailForm.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useIntl } from "react-intl";
 
-import JobDetails from "../JobDetails";
 import { SELECTION_PROCESS } from "../../../../../constants/constants";
 import { mapApiDataToUI } from "../MappedData";
 
