72ea9ec43c080fd20d599522e7fd3715369cb7f8
Kashish8888 <kashish@skizaa.com>
2024-01-19 16:11:12 +0530

    conditionally show create new password

--- src/services/apiServices/hooks/useVerifyOtpAPI.js
+++ src/services/apiServices/hooks/useVerifyOtpAPI.js
@@ -10,28 +10,22 @@ const useVerifyOtpAPI = () => {
   const [verifyOtpResult, setVerifyOtpResult] = useState([]);
   const [apiStatus, setApiStatus] = useState(API_STATUS.IDLE);
 
-  const handleVerifyOtpAPI = async (
-    payload,
-    successCallback,
-    errorCallback
-  ) => {
+  const handleVerifyOtpAPI = async (payload) => {
     try {
       setApiStatus(API_STATUS.LOADING);
       errorWhileResetPassword && setErrorWhileResetPassword("");
       const res = await Http.post(COMPANY_VERIFY_OTP, payload);
-      if (res.status === STATUS_CODES.SUCCESS_STATUS) {
+      if (
+        res.status === STATUS_CODES.SUCCESS_STATUS ||
+        res.code === STATUS_CODES.SUCCESS_STATUS
+      ) {
         setApiStatus(API_STATUS.SUCCESS);
         setVerifyOtpResult(res.data);
-        successCallback(res.data.data);
         return;
-      } 
+      }
       setApiStatus(API_STATUS.ERROR);
-        errorCallback(res);
-      
+      setErrorWhileResetPassword(GENERIC_GET_API_FAILED_ERROR_MESSAGE);
     } catch (err) {
-      const errorMessage =
-        err.response?.data?.message || GENERIC_GET_API_FAILED_ERROR_MESSAGE;
-      errorCallback(errorMessage);
       setApiStatus(API_STATUS.ERROR);
       if (err.response?.data?.message) {
         setErrorWhileResetPassword(err.response?.data?.message);
@@ -48,10 +42,11 @@ const useVerifyOtpAPI = () => {
   return {
     apiStatus,
     errorWhileResetPassword,
+    handleVerifyOtpAPI,
     isError,
     isLoading,
     isSuccess,
-    handleVerifyOtpAPI,
+    setErrorWhileResetPassword,
     verifyOtpResult,
   };
 };
