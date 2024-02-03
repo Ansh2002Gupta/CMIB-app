b1cbd3a7c83bfa85e4200f4de88976661761964d
Kashish8888 <kashish@skizaa.com>
2024-02-03 23:59:50 +0530

    Updated props

--- src/components/TextArea/TextArea.native.js
+++ src/components/TextArea/TextArea.native.js
@@ -4,12 +4,12 @@ import { TextInput } from "@unthinkable/react-core-components";
 import colors from "../../assets/colors";
 import styles from "./TextArea.style";
 
-const TextArea = ({ customStyle, height, ...props }) => {
+const TextArea = ({ customStyle, height, placeholderTextColor, ...props }) => {
   return (
     <TextInput
-      height={84}
+      height={height}
       multiline
-      placeholderTextColor={colors.darkGrey}
+      placeholderTextColor={placeholderTextColor}
       style={{ ...styles.inputStyle, customStyle }}
       {...props}
     />
@@ -19,11 +19,13 @@ const TextArea = ({ customStyle, height, ...props }) => {
 TextArea.defaultProps = {
   customStyle: {},
   height: 84,
+  placeholderTextColor: colors.darkGrey,
 };
 
 TextArea.propTypes = {
   customStyle: PropTypes.object,
   height: PropTypes.number,
+  placeholderTextColor: PropTypes.string,
 };
 
 export default TextArea;
