0fd681ea16d7940e8c4b73cb1d604d57c683ec7b
Kashish8888 <kashish@skizaa.com>
2024-02-12 04:03:05 +0530

    update navigation

--- src/containers/CandidateRoundOne/AddApplication/controller/useAddApplication.js
+++ src/containers/CandidateRoundOne/AddApplication/controller/useAddApplication.js
@@ -2,28 +2,35 @@ import { useIntl } from "react-intl";
 import { useNavigate } from "react-router";
 import { useState } from "react";
 
-import { ADD_APPLICATION_STEPPER, PREVIOUS_SCREEN } from "../../../../constants/constants";
+import {
+  ADD_APPLICATION_STEPPER,
+  PREVIOUS_SCREEN,
+} from "../../../../constants/constants";
 
 const useAddApplication = () => {
-    const intl = useIntl();
-    const navigate = useNavigate()
-    const stepperData = ADD_APPLICATION_STEPPER.map((stepper) => ({
-        id: stepper.id,
-        title: intl.formatMessage({ id: stepper.title }),
-      }));
+  const intl = useIntl();
+  const navigate = useNavigate();
+  const stepperData = ADD_APPLICATION_STEPPER.map((stepper) => ({
+    id: stepper.id,
+    title: intl.formatMessage({ id: stepper.title }),
+  }));
   const [selectedStepper, setSelectedStepper] = useState(stepperData?.[0]);
 
   const onChangeStepper = () => {
-    setSelectedStepper(stepperData[selectedStepper.id]);
-  } 
-  
+    if (selectedStepper.id === ADD_APPLICATION_STEPPER.length) {
+      setSelectedStepper(stepperData[0]);
+    } else {
+      setSelectedStepper(stepperData[selectedStepper.id]);
+    }
+  };
+
   const onClickCancel = () => {
     navigate(PREVIOUS_SCREEN);
-  }
+  };
 
   const onClickBack = () => {
     setSelectedStepper(stepperData[selectedStepper.id - 2]);
-  } 
+  };
 
   return {
     intl,
