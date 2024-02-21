ff78e18b48ba0345126365baa36afcf079dc86b2
Nikhil Sharma <nikhil.sharma@unthinkable.co>
2024-02-21 14:03:01 +0530

    Added BreadCrump

---
+++ src/components/BreadCrumbs/Breadcrumbs.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View } from "@unthinkable/react-core-components";
+
+import CommonText from "../CommonText";
+import CustomTouchableOpacity from "../CustomTouchableOpacity";
+import { navigations } from "../../constants/routeNames";
+import { useNavigate, useLocation } from "../../routes";
+import styles from "./Breadcrumbs.style";
+
+const Breadcrumbs = ({ breadcrumbs }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavigation = (val) => {
+    navigate(val);
+  };
+
+  let isLastElement;
+
+  const getStyles = (index) => {
+    isLastElement = breadcrumbs.length - 1 === index;
+    if (isLastElement) {
+      return styles.disabled;
+    }
+    return styles.enabled;
+  };
+
+  return (
+    <>
+      {location.pathname ===
+        `${navigations.TICKETS}/${navigations.TICKETS_VIEW_EDIT}` && (
+        <View style={styles.container}>
+          {breadcrumbs.map((crumb, index) => {
+            const buttonStyles = getStyles(index);
+            return (
+              <CustomTouchableOpacity
+                disabled={isLastElement}
+                style={styles.container}
+                onPress={() => handleNavigation(crumb.path)}
+              >
+                <CommonText customTextStyle={buttonStyles}>
+                  {crumb.label}
+                </CommonText>
+                {!isLastElement && (
+                  <CommonText customTextStyle={styles.slashStyles}>
+                    /
+                  </CommonText>
+                )}
+              </CustomTouchableOpacity>
+            );
+          })}
+        </View>
+      )}
+    </>
+  );
+};
+
+export default Breadcrumbs;
