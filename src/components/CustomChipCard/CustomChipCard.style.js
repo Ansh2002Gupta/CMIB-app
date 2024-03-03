de6d95ea824a1a3f6c4f73796a492ff857e504a2
Kashish8888 <kashish@skizaa.com>
2024-03-03 15:48:14 +0530

    chip card component

---
+++ src/components/CustomChipCard/CustomChipCard.style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "@unthinkable/react-core-components";
+import colors from "../../assets/colors";
+
+const styles = StyleSheet.create({
+  selectedItemsContainer: {
+    backgroundColor: colors.secondaryGrey,
+    padding: 8,
+    flexDirection: "row",
+    borderRadius: 16,
+    marginTop: 4,
+    marginRight: 4,
+    alignItems: "center",
+  },
+  iconCloseDark: {
+    height: 15,
+    width: 15,
+    marginLeft: 4,
+  },
+});
+export default styles;
