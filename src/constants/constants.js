0f530db6c4b1b0c1b102291227d5426cf08b796c
Kashish8888 <kashish@skizaa.com>
2024-03-07 14:53:03 +0530

    updated max length of input

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -17,6 +17,7 @@ export const CODE_MIN_LENGTH = 2;
 export const CODE_MAX_LENGTH = 8;
 export const NUMBER_MIN_LENGTH = 7;
 export const NUMBER_MAX_LENGTH = 15;
+export const NUMBER_OF_PARTNERS_LENGTH = 3;
 export const MOBILE_NUMBER_MIN_LENGTH = 4;
 export const MOBILE_NUMBER_MAX_LENGTH = 13;
 export const MESSAGE_MAX_LENGTH = 5000;
