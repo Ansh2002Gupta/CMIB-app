dc477df520542350afe3300b6e66c67fe5122d48
Lakshay <lakshay.gupta@unthinkable.co>
2024-03-14 16:32:46 +0530

    removed 401 error check in the company provide save event

--- src/services/apiServices/hooks/CompanyProfile/useGetCompanyProfileAPI.js
+++ src/services/apiServices/hooks/CompanyProfile/useGetCompanyProfileAPI.js
@@ -1,15 +1,11 @@
 import { useState } from "react";
-import { useIntl } from "react-intl";
 
 import useHttpService from "../../../hooks/useHttpService";
-import { useHeader } from "../../../../hooks/useHeader";
 import { API_STATUS, STATUS_CODES } from "../../../../constants/constants";
 import { COMPANY_PROFILE } from "../../apiEndPoint";
 import { GENERIC_GET_API_FAILED_ERROR_MESSAGE } from "../../../../constants/errorMessages";
 
 const useGetCompanyProfileAPI = () => {
-  const { onLogout } = useHeader();
-  const intl = useIntl();
   const [profileResult, setProfileResult] = useState({});
   const [errorWhileGettingResult, setErrorWhileGettingResult] = useState("");
   const [companyProfileApiStatus, setCompanyProfileApiStatus] = useState(
@@ -34,13 +30,6 @@ const useGetCompanyProfileAPI = () => {
       setCompanyProfileApiStatus(API_STATUS.ERROR);
       setErrorWhileGettingResult(res);
     } catch (err) {
-      if (err?.response?.data?.code === STATUS_CODES.UNAUTHORIZED_USER) {
-        await onLogout({
-          message: intl.formatMessage({ id: "label.login_with_new_email" }),
-          isLogoutToast: true,
-          isError: false,
-        });
-      }
       setCompanyProfileApiStatus(API_STATUS.ERROR);
       setErrorWhileGettingResult(
         err.response?.data?.message || GENERIC_GET_API_FAILED_ERROR_MESSAGE
