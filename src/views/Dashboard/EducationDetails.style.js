9cc79630cf6cba4fb56fd6128dee201f12df9281
Monu <monu@daffodilsw.com>
2024-03-19 19:25:28 +0530

    updates

--- src/views/Dashboard/EducationDetails.style.js
+++ src/views/Dashboard/EducationDetails.style.js
@@ -1,10 +1,35 @@
+import colors from "../../assets/colors";
+
 const style = {
   contentContainerStyle: {
     flex: 1,
   },
-
   innerContainerStyle: {
     marginBottom: 16,
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop: 24,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: colors.lightGrey,
+    paddingLeft: 24,
+    paddingRight: 24,
+    backgroundColor: colors.white,
+  },
+  customCardStyle: {
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    paddingRight: 0,
+    paddingLeft: 0,
+    borderWidth: 0,
+    borderRadius: 0,
+    backgroundColor: "transparent",
+  },
+  separatorStyle: {
+    height: 1,
+    backgroundColor: colors.lightGrey,
   },
 };
 
--- src/views/Dashboard/EducationDetailsUI.js
+++ src/views/Dashboard/EducationDetailsUI.js
@@ -30,6 +30,7 @@ const EducationDetailsUI = ({
       <View style={style.innerContainerStyle}>
         <DetailCard
           details={education_detail}
+          customCardStyle={style.customCardStyle}
           headerId={intl.formatMessage({
             id: "label.education_details",
           })}
@@ -37,20 +38,28 @@ const EducationDetailsUI = ({
           handleChange={onChangeValue(education_detail)}
           handleBlur={handleEducationDetailBlur}
         />
+        <View style={style.separatorStyle} />
         <DetailCard
           details={higher_secondary_detail}
+          customCardStyle={style.customCardStyle}
           isEditProfile={isEditable}
           handleChange={onChangeValue(higher_secondary_detail)}
           handleBlur={handleHigherSecondaryDetailBlur}
         />
+        <View style={style.separatorStyle} />
+
         <DetailCard
           details={graduation_detail}
+          customCardStyle={style.customCardStyle}
           isEditProfile={isEditable}
           handleChange={onChangeValue(graduation_detail)}
           handleBlur={handleGraduationDetailBlur}
         />
+        <View style={style.separatorStyle} />
+
         <DetailCard
           details={post_graduation_detail}
+          customCardStyle={style.customCardStyle}
           isEditProfile={isEditable}
           handleChange={onChangeValue(post_graduation_detail)}
           handleBlur={handlePostGraduationDetailBlur}
