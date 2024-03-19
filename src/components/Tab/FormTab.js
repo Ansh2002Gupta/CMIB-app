43274acdc02a61871f9218996487b7794f5f33c3
Monu <monu@daffodilsw.com>
2024-03-19 19:04:33 +0530

    updates

--- src/components/Tab/FormTab.js
+++ src/components/Tab/FormTab.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import {
   Row,
+  ScrollView,
   TouchableOpacity,
   View,
 } from "@unthinkable/react-core-components";
@@ -10,31 +11,35 @@ const FormTab = ({ tabs }) => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   return (
     <View style={styles.container}>
-      <Row gap={12}>
-        {tabs.map((tab, index) => {
-          const { label } = tab;
-          return (
-            <TouchableOpacity
-              onPress={() => setActiveTabIndex(index)}
-              key={index}
-              style={{
-                ...styles.itemContainer,
-                ...(index === activeTabIndex ? styles.activeItemContainer : {}),
-              }}
-            >
-              <CommonText
-                fontWeight={"500"}
-                customTextStyle={{
-                  ...styles.itemText,
-                  ...(index === activeTabIndex ? styles.activeItemText : {}),
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <Row gap={12}>
+          {tabs.map((tab, index) => {
+            const { label } = tab;
+            return (
+              <TouchableOpacity
+                onPress={() => setActiveTabIndex(index)}
+                key={index}
+                style={{
+                  ...styles.itemContainer,
+                  ...(index === activeTabIndex
+                    ? styles.activeItemContainer
+                    : {}),
                 }}
               >
-                {label}
-              </CommonText>
-            </TouchableOpacity>
-          );
-        })}
-      </Row>
+                <CommonText
+                  fontWeight={"500"}
+                  customTextStyle={{
+                    ...styles.itemText,
+                    ...(index === activeTabIndex ? styles.activeItemText : {}),
+                  }}
+                >
+                  {label}
+                </CommonText>
+              </TouchableOpacity>
+            );
+          })}
+        </Row>
+      </ScrollView>
       {tabs[activeTabIndex].component}
     </View>
   );
