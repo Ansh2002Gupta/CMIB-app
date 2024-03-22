7a6f8241586a78673fea759a0e78a6fcae538697
Monu <monu@daffodilsw.com>
2024-03-22 13:19:10 +0530

    updates

---
+++ src/components/Tab/CustomTabs.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import {
+  Row,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "@unthinkable/react-core-components";
+
+import CommonText from "../CommonText";
+import styles from "./CustomTabs.style.js";
+
+export const CustomTabs = ({ tabs, renderHeader }) => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        {renderHeader && renderHeader()}
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <Row gap={24} style={styles.tabsContainer}>
+            {tabs.map((tab, index) => {
+              const { label } = tab;
+              let isActive = index === activeTabIndex;
+              return (
+                <TouchableOpacity
+                  onPress={() => setActiveTabIndex(index)}
+                  key={index}
+                  style={{
+                    ...styles.itemContainer,
+                    ...(isActive ? styles.activeItemContainer : {}),
+                  }}
+                >
+                  <CommonText
+                    fontWeight={isActive ? "600" : "500"}
+                    customTextStyle={{
+                      ...styles.itemText,
+                      ...(isActive ? styles.activeItemText : {}),
+                    }}
+                  >
+                    {label}
+                  </CommonText>
+                </TouchableOpacity>
+              );
+            })}
+          </Row>
+        </ScrollView>
+      </View>
+      {tabs[activeTabIndex].component}
+    </View>
+  );
+};
---
+++ src/components/Tab/FormTabs.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import {
+  Row,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "@unthinkable/react-core-components";
+
+import CommonText from "../CommonText";
+import styles from "./FormTabs.style.js";
+
+export const FormTabs = ({ tabs }) => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <Row gap={12} style={styles.tabContainer}>
+          {tabs.map((tab, index) => {
+            const { label } = tab;
+            return (
+              <TouchableOpacity
+                onPress={() => setActiveTabIndex(index)}
+                key={index}
+                style={{
+                  ...styles.itemContainer,
+                  ...(index === activeTabIndex
+                    ? styles.activeItemContainer
+                    : {}),
+                }}
+              >
+                <CommonText
+                  fontWeight={"500"}
+                  customTextStyle={{
+                    ...styles.itemText,
+                    ...(index === activeTabIndex ? styles.activeItemText : {}),
+                  }}
+                >
+                  {label}
+                </CommonText>
+              </TouchableOpacity>
+            );
+          })}
+        </Row>
+      </ScrollView>
+      {tabs[activeTabIndex].component}
+    </View>
+  );
+};
+
+export default FormTabs;
