3c422c17e4501dcdd9fe777793526a4924919931
Monu <monu@daffodilsw.com>
2024-03-22 14:19:26 +0530

    updates

--- src/components/Tab/CustomTabs.js
+++ src/components/Tab/CustomTabs.js
@@ -1,51 +1,116 @@
 import { useState } from "react";
+import { useIntl } from "react-intl";
 import {
   Row,
   ScrollView,
   TouchableOpacity,
   View,
 } from "@unthinkable/react-core-components";
+import PropTypes from "prop-types";
 
 import CommonText from "../CommonText";
+import ConfirmationModal from "../../containers/ConfirmationModal";
 import styles from "./CustomTabs.style.js";
 
-export const CustomTabs = ({ tabs, renderHeader }) => {
+export const CustomTabs = ({ renderHeader, showWarningOnTabSwitch, tabs }) => {
+  const intl = useIntl();
+
   const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [alertOnTabSwitch, setAlertOnTabSwitch] = useState({
+    showAlert: false,
+    tab: {},
+    tabIndex: -1,
+  });
+
+  const handleTabChange = ({ tab, index }) => {
+    if (index !== activeTabIndex) {
+      if (showWarningOnTabSwitch) {
+        setAlertOnTabSwitch({
+          showAlert: true,
+          tab,
+          tabIndex: index,
+        });
+        return;
+      }
+      setActiveTabIndex(index);
+    }
+  };
+
+  const closeAlertHandler = () => {
+    setAlertOnTabSwitch({
+      showAlert: false,
+      tab: {},
+      tabIndex: -1,
+    });
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        {renderHeader && renderHeader()}
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <Row gap={24} style={styles.tabsContainer}>
-            {tabs.map((tab, index) => {
-              const { label } = tab;
-              let isActive = index === activeTabIndex;
-              return (
-                <TouchableOpacity
-                  onPress={() => setActiveTabIndex(index)}
-                  key={index}
-                  style={{
-                    ...styles.itemContainer,
-                    ...(isActive ? styles.activeItemContainer : {}),
-                  }}
-                >
-                  <CommonText
-                    fontWeight={isActive ? "600" : "500"}
-                    customTextStyle={{
-                      ...styles.itemText,
-                      ...(isActive ? styles.activeItemText : {}),
+    <>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          {renderHeader && renderHeader()}
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <Row gap={24} style={styles.tabsContainer}>
+              {tabs.map((tab, index) => {
+                const { label } = tab;
+                let isActive = index === activeTabIndex;
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      handleTabChange({ tab, index });
+                    }}
+                    key={index}
+                    style={{
+                      ...styles.itemContainer,
+                      ...(isActive ? styles.activeItemContainer : {}),
                     }}
                   >
-                    {label}
-                  </CommonText>
-                </TouchableOpacity>
-              );
-            })}
-          </Row>
-        </ScrollView>
+                    <CommonText
+                      fontWeight={isActive ? "600" : "500"}
+                      customTextStyle={{
+                        ...styles.itemText,
+                        ...(isActive ? styles.activeItemText : {}),
+                      }}
+                    >
+                      {label}
+                    </CommonText>
+                  </TouchableOpacity>
+                );
+              })}
+            </Row>
+          </ScrollView>
+        </View>
+        {tabs[activeTabIndex].component}
       </View>
-      {tabs[activeTabIndex].component}
-    </View>
+      {alertOnTabSwitch?.showAlert && (
+        <ConfirmationModal
+          headingText={intl.formatMessage({
+            id: "label.dirtyFormAlertHeading",
+          })}
+          subHeading={intl.formatMessage({
+            id: "label.dirtyFormAlertSubHeading",
+          })}
+          severity={"warning"}
+          buttonOneText={intl.formatMessage({ id: "label.close" })}
+          buttonTwoText={intl.formatMessage({ id: "label.continue" })}
+          onPressButtonOne={closeAlertHandler}
+          onPressButtonTwo={() => {
+            setActiveTabIndex(alertOnTabSwitch?.tabIndex);
+            closeAlertHandler();
+          }}
+        />
+      )}
+    </>
   );
+};
+
+CustomTabs.propTypes = {
+  renderHeader: PropTypes.func,
+  showWarningOnTabSwitch: PropTypes.bool,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      component: PropTypes.element.isRequired,
+    })
+  ).isRequired,
 };
--- src/components/Tab/FormTabs.js
+++ src/components/Tab/FormTabs.js
@@ -1,51 +1,115 @@
 import { useState } from "react";
+import { useIntl } from "react-intl";
 import {
   Row,
   ScrollView,
   TouchableOpacity,
   View,
 } from "@unthinkable/react-core-components";
+import PropTypes from "prop-types";
 
 import CommonText from "../CommonText";
+import ConfirmationModal from "../../containers/ConfirmationModal";
 import styles from "./FormTabs.style.js";
 
-export const FormTabs = ({ tabs }) => {
+export const FormTabs = ({ showWarningOnTabSwitch, tabs }) => {
+  const intl = useIntl();
+
   const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [alertOnTabSwitch, setAlertOnTabSwitch] = useState({
+    showAlert: false,
+    tab: {},
+    tabIndex: -1,
+  });
+
+  const handleTabChange = ({ tab, index }) => {
+    if (index !== activeTabIndex) {
+      if (showWarningOnTabSwitch) {
+        setAlertOnTabSwitch({
+          showAlert: true,
+          tab,
+          tabIndex: index,
+        });
+        return;
+      }
+      setActiveTabIndex(index);
+    }
+  };
+
+  const closeAlertHandler = () => {
+    setAlertOnTabSwitch({
+      showAlert: false,
+      tab: {},
+      tabIndex: -1,
+    });
+  };
 
   return (
-    <View style={styles.container}>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <Row gap={12} style={styles.tabContainer}>
-          {tabs.map((tab, index) => {
-            const { label } = tab;
-            return (
-              <TouchableOpacity
-                onPress={() => setActiveTabIndex(index)}
-                key={index}
-                style={{
-                  ...styles.itemContainer,
-                  ...(index === activeTabIndex
-                    ? styles.activeItemContainer
-                    : {}),
-                }}
-              >
-                <CommonText
-                  fontWeight={"500"}
-                  customTextStyle={{
-                    ...styles.itemText,
-                    ...(index === activeTabIndex ? styles.activeItemText : {}),
+    <>
+      <View style={styles.container}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <Row gap={12} style={styles.tabContainer}>
+            {tabs.map((tab, index) => {
+              const { label } = tab;
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    handleTabChange({ tab, index });
+                  }}
+                  key={index}
+                  style={{
+                    ...styles.itemContainer,
+                    ...(index === activeTabIndex
+                      ? styles.activeItemContainer
+                      : {}),
                   }}
                 >
-                  {label}
-                </CommonText>
-              </TouchableOpacity>
-            );
+                  <CommonText
+                    fontWeight={"500"}
+                    customTextStyle={{
+                      ...styles.itemText,
+                      ...(index === activeTabIndex
+                        ? styles.activeItemText
+                        : {}),
+                    }}
+                  >
+                    {label}
+                  </CommonText>
+                </TouchableOpacity>
+              );
+            })}
+          </Row>
+        </ScrollView>
+        {tabs[activeTabIndex].component}
+      </View>
+      {alertOnTabSwitch?.showAlert && (
+        <ConfirmationModal
+          headingText={intl.formatMessage({
+            id: "label.dirtyFormAlertHeading",
           })}
-        </Row>
-      </ScrollView>
-      {tabs[activeTabIndex].component}
-    </View>
+          subHeading={intl.formatMessage({
+            id: "label.dirtyFormAlertSubHeading",
+          })}
+          severity={"warning"}
+          buttonOneText={intl.formatMessage({ id: "label.close" })}
+          buttonTwoText={intl.formatMessage({ id: "label.continue" })}
+          onPressButtonOne={closeAlertHandler}
+          onPressButtonTwo={() => {
+            setActiveTabIndex(alertOnTabSwitch?.tabIndex);
+            closeAlertHandler();
+          }}
+        />
+      )}
+    </>
   );
 };
 
-export default FormTabs;
+FormTabs.propTypes = {
+  showWarningOnTabSwitch: PropTypes.bool,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      component: PropTypes.element.isRequired,
+    })
+  ).isRequired,
+};
