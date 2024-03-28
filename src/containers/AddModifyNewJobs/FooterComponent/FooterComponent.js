443c9e46c417f4c2721edeceda8725016b50ecac
usl-sz-0791 <usl-sz-0791@supports-MacBook-Pro.local>
2024-03-28 14:03:06 +0530

    removed unused compoennt

--- src/containers/AddModifyNewJobs/FooterComponent/FooterComponent.js
+++ src/containers/AddModifyNewJobs/FooterComponent/FooterComponent.js
@@ -11,7 +11,6 @@ import styles from "./FooterComponent.styles";
 const FooterComponent = ({
   isWebView,
   isCheckList,
-  isMessageVisible,
   setIsCheckList,
   onSubmit,
 }) => {
@@ -19,20 +18,18 @@ const FooterComponent = ({
   const intl = useIntl();
   return (
     <View style={styles.containerStyle(isWebView)}>
-      {isMessageVisible && (
-        <View style={styles.firstViewStyles}>
-          <View style={styles.checkBoxViewStyle}>
-            <CheckBox
-              title={intl.formatMessage({ id: "label.do_not_send_email" })}
-              handleCheckbox={(vale) => {
-                setIsCheckList((prev) => !prev);
-              }}
-              id={`isCheckList${isCheckList}`}
-              isSelected={isCheckList}
-            />
-          </View>
+      <View style={styles.firstViewStyles}>
+        <View style={styles.checkBoxViewStyle}>
+          <CheckBox
+            title={intl.formatMessage({ id: "label.do_not_send_email" })}
+            handleCheckbox={(vale) => {
+              setIsCheckList((prev) => !prev);
+            }}
+            id={`isCheckList${isCheckList}`}
+            isSelected={isCheckList}
+          />
         </View>
-      )}
+      </View>
       <View style={styles.getSecondViewStyle(isWebView)}>
         <View style={styles.buttonViewStyle}>
           <CustomButton
