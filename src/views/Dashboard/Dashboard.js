ee1a085bc0bb8cae233f5eb0a4b5ff988cf00d37
abhayagrahary7080 <abhay.agrahary@unthinkable.co>
2024-04-04 17:59:24 +0530

    Dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -4,7 +4,6 @@ import { View } from "@unthinkable/react-core-components";
 
 import CommonText from "../../components/CommonText";
 import RangeSlider from "../../components/RangeSlider";
-import SavedJobComponent from "../../components/SavedJobComponent/SavedJobComponent";
 import UploadImage from "../../components/UploadImage";
 import useSaveLogo from "../../services/apiServices/hooks/CompanyLogo/useSaveLogoAPI";
 import styles from "./dashboard.style";
@@ -42,7 +41,7 @@ function DashboardView() {
         {intl.formatMessage({ id: "label.dashboard" })}
       </CommonText>
       <View>
-        {/* <UploadImage
+        <UploadImage
           {...{
             onDeleteImage,
             errorWhileUpload,
@@ -53,8 +52,7 @@ function DashboardView() {
             setFileUploadResult,
             uploadPercentage,
           }}
-        /> */}
-        <SavedJobComponent />
+        />
       </View>
       <View>
         <CommonText customTextStyle={styles.header}>
