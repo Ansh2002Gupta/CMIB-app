6ca7a7b90a8477dbf8204b4e7d4c8a0c366a936a
Nikhil Sharma <nikhil.sharma@unthinkable.co>
2024-04-04 17:43:54 +0530

    Fixed popmessage

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,18 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import { Platform } from "@unthinkable/react-core-components";
+const isWeb = Platform.OS.toLowerCase() === "web";
 
 const useOutsideClick = (ref, callback) => {
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (ref.current && !ref.current.contains(event.target)) {
-        callback();
-      }
-    };
+    if (isWeb) {
+      const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+          callback();
+        }
+      };
 
-    document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside);
 
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
   }, [ref, callback]);
 };
 
