900e4b70f9c47cc16c036c122b4e5bc59b60c8c1
Lakshay <lakshay.gupta@unthinkable.co>
2024-04-05 19:43:27 +0530

    fixed pdf images not showing on the UI

--- src/images/index.js
+++ src/images/index.js
@@ -236,6 +236,10 @@ const iconBriefcase = appendAppBaseURl("/images/svg/iconBriefcase.svg");
 const iconLocation = appendAppBaseURl("/images/svg/iconLocation.svg");
 const iconRupee = appendAppBaseURl("/images/svg/iconRupee.svg");
 const videoPlayIcon = appendAppBaseURl("/images/svg/videoPlayIcon.svg");
+const pdfIcon = appendAppBaseURl("/images/svg/pdfIcon.svg");
+const rightDiagonalArrow = appendAppBaseURl(
+  "/images/svg/rightDiagonalArrow.svg"
+);
 
 const images = {
   iconArrowUp,
@@ -405,6 +409,8 @@ const images = {
   iconLocation,
   iconRupee,
   videoPlayIcon,
+  pdfIcon,
+  rightDiagonalArrow,
 };
 
 export default images;
