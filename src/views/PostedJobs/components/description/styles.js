f9f3a33fc1a0bb63232d8d1685cbeb866510eb4b
abhayagrahary7080 <abhay.agrahary@unthinkable.co>
2024-04-15 15:17:16 +0530

    Job Description

--- src/views/PostedJobs/components/description/styles.js
+++ src/views/PostedJobs/components/description/styles.js
@@ -37,6 +37,6 @@ const styles = {
     marginTop: 16,
   },
   quilStyle: { height: "auto", borderWidth: 0 },
-  quillContainerStyle: { borderWidth: 0, backgroundColor: "blue" },
+  quillContainerStyle: { borderWidth: 0 },
 };
 export default styles;
