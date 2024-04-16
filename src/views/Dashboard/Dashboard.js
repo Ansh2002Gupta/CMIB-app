9cf4f6a4ac2f43ffa20b0cb732962c8b4a81e164
Ansh Gupta <ansh.gupta@unthinkable.co>
2024-04-17 01:56:57 +0530

    PR bugs resolved.

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -2,77 +2,43 @@ import React, { useContext, useState } from "react";
 import { useIntl } from "react-intl";
 import { View } from "@unthinkable/react-core-components";
 
-import MultiColumn from "../../core/layouts/MultiColumn";
+import { TwoRow } from "../../core/layouts";
 
-import CommonText from "../../components/CommonText";
-import CustomCell from "../../components/CustomCell/";
-import TouchableImage from "../../components/TouchableImage";
-import SearchView from "../../components/SearchView";
+import CAJobsDashboard from "../CAJobsDashboard";
+import IconHeader from "../../components/IconHeader/IconHeader";
 import { SideBarContext } from "../../globalContext/sidebar/sidebarProvider";
-import images from "../../images";
+import useIsWebView from "../../hooks/useIsWebView";
+import { moduleKeys } from "../../constants/sideBarHelpers";
 import styles from "./dashboard.style";
 
 // Just ignore this file as just to test custom component
 function DashboardView() {
   const intl = useIntl();
-  const [isEnabled, setIsEnabled] = useState(false);
-
-  const dataList = ["Apple", "Banana", "Orange", "Mango", "Pineapple", "Grape"];
-  const FilterIcon = images.iconFilter;
-  const MoreIcon = images.iconMore;
-  const AddIcon = images.iconAdd;
-
-  const toggleSwitch = () => {
-    setIsEnabled((previousState) => !previousState);
-  };
-
-  const handleSearchResults = (filteredData) => {};
-
-  const searchData = [
-    {
-      content: <SearchView data={dataList} onSearch={handleSearchResults} />,
-      style: {},
-      isFillSpace: true,
-    },
-    {
-      content: (
-        <TouchableImage
-          source={FilterIcon}
-          parentStyle={styles.imageParentStyle}
-        />
-      ),
-      style: {},
-      isFillSpace: false,
-    },
-    {
-      content: (
-        <TouchableImage
-          source={MoreIcon}
-          disabled={false}
-          isSelector={true}
-          parentStyle={styles.imageParentStyle}
-        />
-      ),
-      style: {},
-      isFillSpace: false,
-    },
-  ];
+  const [sideBarState] = useContext(SideBarContext);
+  const { selectedModule } = sideBarState;
+  const { isWebView } = useIsWebView();
 
   return (
     <View style={styles.container}>
-      <CommonText customTextStyle={styles.header}>
-        {intl.formatMessage({ id: "label.dashboard" })}
-      </CommonText>
-      <MultiColumn columns={searchData} />
-      <CustomCell
-        onPress={toggleSwitch}
-        title={"AddDesignation"}
-        isLeft
-        isSvg
-        leftSource={AddIcon}
-        style={styles.customCellStyle}
-        textStyle={styles.customCellTextStyle}
-      />
+      <TwoRow
+        topSection={
+          isWebView && (
+            <IconHeader
+              hasIconBar
+              headerText={intl.formatMessage({ id: "label.dashboard" })}
+              intl={intl}
+            />
+          )
+        }
+        isBottomFillSpace
+        bottomSection={
+          <>
+            {moduleKeys.CA_JOBS_KEY === selectedModule?.key ? (
+              <CAJobsDashboard />
+            ) : null}
+          </>
+        }
+      ></TwoRow>
     </View>
   );
 }
